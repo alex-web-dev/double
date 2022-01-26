80075170522faaa8f90c46092a7da248d2543730
Alex <lesha.rybalko.1998@mail.ru>
2022-01-26 16:48:38 +0300

    add adaptive to index page

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,8 @@
+const $header = document.querySelector('.header');
+const $headerMenu = $header.querySelector('.header__mobile-menu');
+const $menuToggle = $headerMenu.querySelector('.mobile-menu__toggle');
+
+$menuToggle.addEventListener('click', () => {
+  $headerMenu.classList.toggle('mobile-menu_active');
+  document.body.classList.toggle('body_lock');  
+});
