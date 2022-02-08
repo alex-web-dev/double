2d9a7aebd79176169efd393a2449649687c44253
Alexey <alex.web-dev@mail.ru>
2022-02-08 13:47:48 +0300

    add 404 page

--- src/js/run-line.js
+++ src/js/run-line.js
@@ -1,13 +1,27 @@
 import Marquee3k from 'marquee3000';
 
 window.addEventListener('load', () => {
-  const $marquee3k = document.querySelector('.marquee3k');
+  setRunSpeed({
+    selector: '.marquee3k-contacts',
+    desktopSpeed: 3.5,
+    mobileSpeed: 1.5
+  });
+
+  setRunSpeed({
+    selector: '.marquee3k-404',
+    desktopSpeed: 2.8,
+    mobileSpeed: 1
+  });
+
+  Marquee3k.init();
+});
+
+function setRunSpeed({selector, desktopSpeed = 3, mobileSpeed = 2}) {
+  const $marquee3k = document.querySelector(selector);
 
   if ($marquee3k) {
     const isDesktop = window.innerWidth > 1280;
 
-    $marquee3k.dataset.speed = isDesktop ? '3.5' : '1.5';
-    
-    Marquee3k.init();
+    $marquee3k.dataset.speed = isDesktop ? desktopSpeed : mobileSpeed;
   }
-});
+}
